17ca31c9e4c39af83611f619c2e7b3441285b609
Maxim Davydov <norm.pochta.maxa@gmail.com>
2024-04-26 15:10:16 +0300

    Update test cases structure

--- hw/code/test_commerce_center.py
+++ hw/code/test_commerce_center.py
@@ -1,4 +1,7 @@
 from base import BaseCase
+from ui.pages.commerce_center_page import CommerceCenterPage
+
+import time
 class TestCommerceCenter(BaseCase):
-    def test_test(self):
-        assert 1 == 1
+    def test_test(self, commerce_center_page: CommerceCenterPage):
+        assert commerce_center_page.is_opened()
---
+++ hw/code/ui/pages/commerce_center_page.py
@@ -0,0 +1,4 @@
+from .base_page import BasePage
+
+class CommerceCenterPage(BasePage):
+    url = 'https://ads.vk.com/hq/ecomm/catalogs'
