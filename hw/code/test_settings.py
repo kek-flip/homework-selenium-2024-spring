139045c93311a885ceadd2d4bcbe3ab798afef26
tomsuliana <ultomskaya@yandex.ru>
2024-05-29 14:23:48 +0300

    Old tests removed

--- hw/code/test_settings.py
+++ hw/code/test_settings.py
@@ -13,45 +13,45 @@ class TestSettingsPage(BaseCase):
     FIO = "Шенфельдт Софья Андреевна"
     VALID_INN = "444444444444"
 
-    # def test_fill_data(self, settings_page: SettingsPage):
-    #     settings_page.fill_phone(self.VALID_PHONE)
-    #     assert settings_page.is_save_btn_visible()
-    #     assert settings_page.is_cancel_btn_visible()
+    def test_fill_data(self, settings_page: SettingsPage):
+        settings_page.fill_phone(self.VALID_PHONE)
+        assert settings_page.is_save_btn_visible()
+        assert settings_page.is_cancel_btn_visible()
 
-    # def test_must_have_field(self, settings_page: SettingsPage):
-    #     settings_page.fill_phone(self.VALID_PHONE)
-    #     settings_page.click_save()
-    #     assert settings_page.is_must_have_field_visible()
+    def test_must_have_field(self, settings_page: SettingsPage):
+        settings_page.fill_phone(self.VALID_PHONE)
+        settings_page.click_save()
+        assert settings_page.is_must_have_field_visible()
 
-    # def test_invalid_phone(self, settings_page: SettingsPage):
-    #     settings_page.fill_phone(self.INVALID_PHONE)
-    #     settings_page.click_save()
-    #     assert settings_page.is_invalid_phone_visible()
+    def test_invalid_phone(self, settings_page: SettingsPage):
+        settings_page.fill_phone(self.INVALID_PHONE)
+        settings_page.click_save()
+        assert settings_page.is_invalid_phone_visible()
 
-    # def test_extra_email(self, settings_page: SettingsPage):
-    #     settings_page.click_add_email()
-    #     assert settings_page.is_extra_email_field_visible()
+    def test_extra_email(self, settings_page: SettingsPage):
+        settings_page.click_add_email()
+        assert settings_page.is_extra_email_field_visible()
 
-    # def test_invalid_email(self, settings_page: SettingsPage):
-    #     settings_page.click_add_email()
-    #     settings_page.fill_email(self.INVALID_EMAIL)
-    #     settings_page.click_save()
-    #     assert settings_page.is_invalid_email_visible()
+    def test_invalid_email(self, settings_page: SettingsPage):
+        settings_page.click_add_email()
+        settings_page.fill_email(self.INVALID_EMAIL)
+        settings_page.click_save()
+        assert settings_page.is_invalid_email_visible()
 
-    # def test_invalid_inn(self, settings_page: SettingsPage):
-    #     settings_page.fill_inn(self.INVALID_INN)
-    #     settings_page.click_save()
-    #     assert settings_page.is_invalid_inn_visible()
+    def test_invalid_inn(self, settings_page: SettingsPage):
+        settings_page.fill_inn(self.INVALID_INN)
+        settings_page.click_save()
+        assert settings_page.is_invalid_inn_visible()
 
-    # def test_small_inn(self, settings_page: SettingsPage):
-    #     settings_page.fill_inn(self.SMALL_INN)
-    #     settings_page.click_save()
-    #     assert settings_page.is_small_inn_visible()
+    def test_small_inn(self, settings_page: SettingsPage):
+        settings_page.fill_inn(self.SMALL_INN)
+        settings_page.click_save()
+        assert settings_page.is_small_inn_visible()
 
-    # def test_language(self, settings_page: SettingsPage):
-    #     settings_page.select_language(self.LANGUAGE)
-    #     assert settings_page.is_english_visible()
-    #     settings_page.select_language(self.RUSSIAN_LANGUAGE)
+    def test_language(self, settings_page: SettingsPage):
+        settings_page.select_language(self.LANGUAGE)
+        assert settings_page.is_english_visible()
+        settings_page.select_language(self.RUSSIAN_LANGUAGE)
 
     def test_change_settings(self, settings_page: SettingsPage):
         settings_page.fill_fio(self.FIO)
